1af19fcb71554b02c7873124b33ab653cb306e68
Caryn McCarthy <cmccarthy15@gmail.com>
2020-02-21 15:18:31 -0500

    add meme controller update

--- app/controllers/api/v1/memes_controller.rb
+++ app/controllers/api/v1/memes_controller.rb
@@ -22,6 +22,25 @@ class Api::V1::MemesController < ApplicationController
         else
           render json: {errors: @meme.errors.full_messages[0]}
         end
+    end
+    
+    def update
+      @meme = Meme.find(params[:id])
+      if params[:url]
+        @meme.update({ url: params[:url] })
       end
+      if params[:title]
+        @meme.update({ title: params[:title] })
+      end
+      if params[:subreddit]
+        @meme.update({ subreddit_id: params[:subreddit].to_i })
+      end
+
+      if @meme.save
+        render json: @meme, status: :ok
+      else
+        render json: {errors: @meme.errors.full_messages[0]}
+      end
+    end
 
 end
